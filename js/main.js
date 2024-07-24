59078b99492fff4a1f3c13ee24334064808ed529
gracecavanagh <151089198+gracecavanagh@users.noreply.github.com>
2024-07-23 23:42:53 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -126,13 +126,13 @@
 
 function myFunction(imgs) {
   // Get the expanded image
-  var expandImg = document.getElementById("expandedImg");
+  var gexpandImg = document.getElementById("gexpandedImg");
   // Get the image text
-  var imgText = document.getElementById("imgtext");
+  var gimgText = document.getElementById("gimgtext");
   // Use the same src in the expanded image as the image being clicked on from the grid
-  expandImg.src = imgs.src;
+  gexpandImg.src = imgs.src;
   // Use the value of the alt attribute of the clickable image as text inside the expanded image
-  imgText.innerHTML = imgs.alt;
+  gimgText.innerHTML = imgs.alt;
   // Show the container element (hidden with CSS)
-  expandImg.parentElement.style.display = "block";
+  gexpandImg.parentElement.style.display = "block";
 }
